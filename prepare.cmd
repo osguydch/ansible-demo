65bba62229f8415d0bf8fe4bc8230a0a963986c5
David Cheng <chengguangwei@huawei.com>
2019-01-24 09:29:33 +0800

    Update prepare.cmd
    Add vagrant install script

--- prepare.cmd
+++ prepare.cmd
@@ -1,3 +1,5 @@
+安装 Vritualbox 
+
 配置yum源：
 
 vim /etc/yum.repos.d/virtualbox.repo
@@ -14,3 +16,19 @@ gpgkey=https://www.virtualbox.org/download/oracle_vbox.asc
 
 执行：
 yum -y install VirtualBox-5.1
+
+安装 vagrant.
+----------- For 64-bit machine -----------
+# yum -y install https://releases.hashicorp.com/vagrant/1.9.6/vagrant_1.9.6_x86_64.rpm
+----------- For 32-bit machine ----------- 
+# yum -y install https://releases.hashicorp.com/vagrant/1.9.6/vagrant_1.9.6_i686.rpm
+
+# mkdir ~/vagrant-home 
+# cd ~/vagrant-home 
+
+----------- Installing Ubuntu -----------
+# vagrant init ubuntu/xenial64
+----------- Installing CentOS -----------
+# vagrant init centos/7
+
+# vagrant up
